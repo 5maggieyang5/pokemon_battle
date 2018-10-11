252aad556101f849707230f2a17557f33a8dd092
mag <mag.it.use@gmail.com>
2018-10-11 05:42:05 +0000

    added all the needed functions and edit the README file

--- README.md
+++ README.md
@@ -1,17 +1,32 @@
 # Pokemon_Battle Project
 
-Use Pokéapi (https://pokeapi.co/) to evaluate the type advantages of two Pokémon. It takes two Pokémon and determine which one has the favourable type advantage.
-- In the case of no type advantage the “winner” should be the Pokémon with the highest base stats.
-- In the case of a tie the “winner” should be the first Pokémon passed to it.
-
-## Final Product
-
+Use Pokéapi (https://pokeapi.co/) to evaluate the type advantages of two Pokémon.
+It takes two Pokémon and determine which one has the favourable type advantage.
+- In the case of no type advantage the “winner” will be the Pokémon with the highest base stats.
+- In the case of a tie the “winner” will be the first Pokémon passed to it.
 
 ## Dependencies
 
+- "node-fetch": "^2.2.0"
 
 ## Getting Started
 
 - Install all dependencies (using the `npm install` command).
-- Run the development web server using the `npm start` command.
+- Run the app using the `node pokemon_battle.js pokemon1_name pokemon2_name` command. (Example: `node pokemon_battle.js bulbasaur charmander`)
 
+## Thought Process
+
+1. Check user input see whether two Pokémon name provided.
+2. Use Pokémon name do API call to get Pokémon infor:
+   Name & Types & Stats.
+3. Use Pokémon type url do API call to get damage relations:
+   double_damage_to & half_damage_to & no_damage_to
+4. Use damage relations to find which Pokémon has the favourable
+   type advantage:
+   - initial               : damage = 1
+   - if in double_damage_to: damage * 2
+   - if in half_damage_to  : damage * 0.5
+   - if in no_damage_to    : damage * 0
+5. If no type advantage, check Pokémon total stats. “Winner” will
+   be the Pokémon with the highest base stats.
+6. In the case of a tie the “winner” will be the first Pokémon passed to it.
--- pokemon_battle.js
+++ pokemon_battle.js
@@ -1,8 +1,12 @@
 const fetch           = require("node-fetch");
 const choosedPokemons = process.argv.slice(2);
+const pokemon1        = choosedPokemons[0];
+const pokemon2        = choosedPokemons[1];
 const initialUrl      = "https://pokeapi.co/api/v2/";
 
-// ----- Check user input of choosed pokemons ----- //
+  // ------------------------------------------------ //
+ //       Check user input of choosed pokemons       //
+// ------------------------------------------------ //
 
 const checkInput = choosedPokemons => {
   if (choosedPokemons[0] && choosedPokemons[1]) {
@@ -12,7 +16,9 @@ const checkInput = choosedPokemons => {
   }
 };
 
-// ----- Get both pokemons basic infor ----- //
+  // ------------------------------------------------ //
+ //            Get pokemon's basic infor             //
+// ------------------------------------------------ //
 
 async function getPokemonsInfor(pokemonName) {
 
@@ -28,27 +34,166 @@ async function getPokemonsInfor(pokemonName) {
   }
 }
 
-let promises = [];
-choosedPokemons.map((pokemon) => {
-  promises.push(getPokemonsInfor(pokemon));
-})
+  // ------------------------------------------------ //
+ //          Get pokemons damage relations           //
+// ------------------------------------------------ //
 
-Promise.all(promises)
-  .then((results) => {
-    let pokemon = [];
-    for (let i = 0; i < results.length; i++) {
-      pokemon.push(results[i].pokemon)
-    }
-    console.log("-----Pokemons Infor: ", pokemon);
+async function getPokemonsDamageRelations(typeURL) {
+
+  let response         = await fetch(typeURL);
+  let json             = await response.json();
+  let damageRelation   = json.damage_relations;
+  let double_damage_to = damageRelation.double_damage_to;
+  let half_damage_to   = damageRelation.half_damage_to;
+  let no_damage_to     = damageRelation.no_damage_to;
+
+  let damageX2 = [];
+  for(let damageType of double_damage_to) {
+    damageX2.push(damageType.name)
+  };
+
+  let damageXhalf = [];
+  for(let damageType of half_damage_to) {
+    damageXhalf.push(damageType.name)
+  };
+
+  let damageXzero = [];
+  for(let damageType of no_damage_to) {
+    damageXzero.push(damageType.name)
+  };
+
+  return {
+    damageX2: damageX2,
+    damageXhalf: damageXhalf,
+    damageXzero: damageXzero
+  }
+}
+
+  // ------------------------------------------------ //
+ //              Check pokemon stats                 //
+// ------------------------------------------------ //
+
+const checkTotalStats = stats => {
+  let totalStats = 0;
+  stats.forEach(stat => {
+    totalStats += stat.base_stat;
   })
-  .catch(error => console.error(error));
+  return totalStats;
+}
 
-// ----- RUN FUNCTION ----- //
+  // ------------------------------------------------ //
+ //             Check type of advantage              //
+// ------------------------------------------------ //
+
+const typeOfAdvantage = (pokemon1Types, pokemon2Types) => {
+  let p1 = 1;
+  let p2 = 1;
+
+  pokemon1Types.forEach( p1Type => {
+    pokemon2Types.forEach( p2Type => {
+      let p1damageX2    = p1Type.damageRelations.damageX2;
+      let p1damageXhalf = p1Type.damageRelations.damageXhalf;
+      let p1damageXzero = p1Type.damageRelations.damageXzero;
+
+      let p2damageX2    = p2Type.damageRelations.damageX2;
+      let p2damageXhalf = p2Type.damageRelations.damageXhalf;
+      let p2damageXzero = p2Type.damageRelations.damageXzero;
+
+      if(p1damageX2.indexOf(p2Type.name) !== -1) {
+        p1 *= 2;
+      }
+      if(p1damageXhalf.indexOf(p2Type.name) !== -1) {
+        p1 *= 0.5;
+      }
+      if(p1damageXzero.indexOf(p2Type.name) !== -1) {
+        p1 *= 1;
+      }
+      if(p2damageX2.indexOf(p1Type.name) !== -1) {
+        p2 *= 2;
+      }
+      if(p2damageXhalf.indexOf(p1Type.name) !== -1) {
+        p2 *= 0.5;
+      }
+      if(p2damageXzero.indexOf(p1Type.name) !== -1) {
+        p2 *= 1;
+      }
+    })
+  })
+
+  return p1 === p2 ? "no type advantage"
+       : p1 > p2 ? "p1 win" : "p2 win";
+}
+
+  // ------------------------------------------------ //
+ //             Initial data function                //
+// ------------------------------------------------ //
+
+async function init(){
+
+  let pokemon1Infor = await getPokemonsInfor(pokemon1);
+  let pokemon2Infor = await getPokemonsInfor(pokemon2);
+  let pokemon1Totalstats = checkTotalStats(pokemon1Infor.pokemon.stats);
+  let pokemon2Totalstats = checkTotalStats(pokemon2Infor.pokemon.stats);
+
+  let pokemon1Types = [];
+  for(let pokemon1Type of pokemon1Infor.pokemon.types) {
+    pokemon1Types.push(pokemon1Type.type)
+  };
+
+  let pokemon2Types = [];
+  for(let pokemon2Type of pokemon2Infor.pokemon.types) {
+    pokemon2Types.push(pokemon2Type.type)
+  };
+
+  for (let poke1Type of pokemon1Types) {
+    let pokemon1DamageRelations = await getPokemonsDamageRelations(poke1Type.url);
+    poke1Type.damageRelations = pokemon1DamageRelations;
+  }
+
+  for (let poke2Type of pokemon2Types) {
+    let pokemon2DamageRelations = await getPokemonsDamageRelations(poke2Type.url);
+    poke2Type.damageRelations = pokemon2DamageRelations;
+  }
+
+  return {
+    pokemon1Infor: pokemon1Infor,
+    pokemon2Infor: pokemon2Infor,
+    pokemon1Totalstats: pokemon1Totalstats,
+    pokemon2Totalstats: pokemon2Totalstats,
+    pokemon1Types: pokemon1Types,
+    pokemon2Types: pokemon2Types
+  }
+}
+
+  // ------------------------------------------------ //
+ //                  RUN FUNCTION                    //
+// ------------------------------------------------ //
 
 const run = () => {
   console.log('Welcome to the Pokemon Battle!');
   checkInput(choosedPokemons);
-
+  init()
+  .then(results => {
+    return typeOfAdvantage(results.pokemon1Types, results.pokemon2Types);
+  })
+  .then(results => {
+    if (results === "p1 win") {
+      console.log(`${pokemon1} has the favourable type advantage`)
+    } else if (results === "p2 win"){
+      console.log(`${pokemon2} has the favourable type advantage`)
+    } else {
+      init().then(results => {
+        if (results.pokemon1Totalstats === results.pokemon2Totalstats) {
+          console.log(`${pokemon1} is the first pokemon`);
+        } else if (results.pokemon1Totalstats > results.pokemon2Totalstats) {
+          console.log(`${pokemon1} has the highest base stats`)
+        } else {
+          console.log(`${pokemon2} has the highest base stats`)
+        }
+      })
+    }
+  })
+  .catch( error => console.error(error))
 }
 
 run();
